ecad2ee83c9569b54ff05116c8d1efebc7914c72
osha7 <62903311+osha7@users.noreply.github.com>
2020-05-30 13:00:14 -0600

    Add files via upload

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+        set :session_secret, "we_change"
+  end
+
+  get "/" do
+    erb :welcome
+  end
+
+  
+
+  
+
+  helpers do
+  end
+
+
+end
