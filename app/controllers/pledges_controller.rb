5530e26abda42d794dc2efb2e367d4d1f652e5fa
osha7 <ogroetz@gmail.com>
2020-06-01 10:09:03 -0600

    pledges added

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,6 +1,26 @@
 
 class PledgesController < ApplicationController
 
-    
+    get '/pledge/1' do
+        erb :'/pledges/pledge_1'
+    end
+
+    post '/pledge/1' do
+    end
+
+    get '/pledge/2' do
+        erb :'/pledges/pledge_2'
+    end
+
+    post '/pledge/2' do
+    end
+
+    get '/pledge/3' do
+        erb :'/pledges/pledge_3'
+    end
+
+    post '/pledge/3' do
+    end
+
 
 end
